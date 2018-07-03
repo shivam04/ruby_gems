dcf651b8712bac2d2a04ffb67e7e68693a1af8ec
Shivam Sinha <shivams@qubole.com>
2018-07-03 14:24:59 +0530

    algo

---
+++ algorithms/algorithms.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |s|
+  s.name = "algorithms"
+  s.version = "0.0.5"
+  s.date = "2018-07-03"
+  s.summary = "Contains various algorithms"
+  s.authors = ["Shivam Sinha"]
+  s.files = ['lib/algorithms.rb']
+  s.require_paths = ["lib"]
+end
--- test.rb
+++ test.rb
@@ -1,7 +1,11 @@
 require 'my_gem'
 require 'awesome_gem'
 require 'coolify/string'
+require 'algorithms'
 
 MyGem::Calculator.add(4,5)
 AwesomeGem::WhoIs.awesome?
 puts "My name is shivam sinha".coolify
+puts Algorithms::BasicAlgo.max(5,6)
+puts Algorithms::BasicAlgo.min(5,6)
+
